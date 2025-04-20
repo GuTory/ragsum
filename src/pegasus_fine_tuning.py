b9c8284253b10e54549b82436c6fe1752ed7b4b0
GuTory <kristof.toth@student.maastrichtuniversity.nl>
2025-04-20 13:22:31 +0000

    epoch increase

--- src/pegasus_fine_tuning.py
+++ src/pegasus_fine_tuning.py
@@ -32,7 +32,7 @@ if __name__ == "__main__":
         per_device_eval_batch_size=4,
         weight_decay=0.01,
         save_total_limit=3,
-        num_train_epochs=1,
+        num_train_epochs=5,
         predict_with_generate=True,
         warmup_steps=100,
         max_steps=50,
